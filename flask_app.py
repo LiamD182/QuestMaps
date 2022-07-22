aa9f48e50df57cf463dc19a2d9f69cd23137201b
Liam Dale <liamdale182@gmail.com>
2022-07-22 10:59:08 +0000

    Adding Quest to the datavbase.

--- flask_app.py
+++ flask_app.py
@@ -1,18 +1,46 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+#https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "random string"
 
+
+import folium
+
 db = SQLAlchemy(app)
+
+class user(db.Model):
+    id = db.Column('user_id', db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(60))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+class quest(db.Model):
+    id = db.Column('quest_id', db.Integer, primary_key = True)
+    author = db.Column(db.String(100))
+    quest_name = db.Column(db.String(100))
+
+    def __init__(self, author, quest_name):
+        self.author = author
+        self.quest_name = quest_name
+
+#class quest_location(db.Model):
+#    id = db.Column('quest_id', db.Integer, primary_key = True)
+
+
+
 
 class students(db.Model):
     id = db.Column('student_id', db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     city = db.Column(db.String(50))
-    addr = db.Column(db.String(200)) 
+    addr = db.Column(db.String(200))
     pin = db.Column(db.String(10))
 
     def __init__(self, name, city, addr,pin):
@@ -21,7 +49,26 @@ class students(db.Model):
         self.addr = addr
         self.pin = pin
 
+
 @app.route('/')
+def index():
+     return render_template('index.html' )
+
+@app.route('/map')
+def map():
+    start_coords = (46.9540700, 142.7360300)
+    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    return folium_map._repr_html_()
+
+
+@app.route('/start')
+def start():
+    db.create_all()
+    new_quest = quest(author ='Liam', quest_name = 'UNESCO')
+    db.session.add(new_quest)
+    db.session.commit()
+
+@app.route('/db')
 def show_all():
     return render_template('show_all.html', students = students.query.all() )
 
@@ -33,7 +80,7 @@ def new():
         else:
             student = students(request.form['name'], request.form['city'],
                 request.form['addr'], request.form['pin'])
-            
+
             db.session.add(student)
             db.session.commit()
             flash('Record was successfully added')
